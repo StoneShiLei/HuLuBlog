e43bc941782352e4f6fede934a200e82c4ce3d83
StoneShiLei <569812422@qq.com>
2021-02-27 18:29:35 +0800

    升级项目到4.7.2 framework版本，升级全部三方引入库版本至4.7.2 添加rabbitmq项目

---
+++ Com.Stone.HuLuBlog.EmailService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Stone.HuLuBlog.EmailService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
