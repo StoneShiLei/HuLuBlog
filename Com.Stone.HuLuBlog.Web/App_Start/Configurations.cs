5427338b2cd4ed688f7fb5562a712b76beada526
StoneShiLei <569812422@qq.com>
2021-03-03 23:24:09 +0800

    修改config

--- Com.Stone.HuLuBlog.Web/App_Start/Configurations.cs
+++ Com.Stone.HuLuBlog.Web/App_Start/Configurations.cs
@@ -12,7 +12,7 @@ namespace Com.Stone.HuLuBlog.Web.App_Start
         public const string COMMENT_KEY = "HULUBLOG_COMMENT";
         public const int TOKEN_TIME = 30;  //token 过期时间 单位分钟
         public const string UPLOAD_PATH = "~/Upload/Images/";
-        public const string MQ_CONNSTR = "host=localhost;PublisherConfirms=false;persistentMessages=true;prefetchcount=50;username=用户名;password=密码";
+        public const string MQ_CONNSTR = "host=localhost;PublisherConfirms=false;persistentMessages=true;prefetchcount=50;username=guest;password=guest";
         public const string HuLuEmail = "569812422@qq.com";
         public const string BlogEmail = "hulu@hafuhafu.cn";
     }
